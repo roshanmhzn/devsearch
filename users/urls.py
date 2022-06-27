6dc271f352c4fcaaea659f8a3635b41a446e32cc
Maharjan <roshan.maharjan@cotiviti.com>
2022-06-27 16:34:42 +0545

    Added Skills models, added user-profile.html, set views and configured urls

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name="profiles"),
+    path('profile/<str:pk>/', views.userProfile, name="user_profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import get_object_or_404, render
 
 from .models import Profile
 
@@ -6,3 +7,11 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
+
+
+def userProfile(request, pk):
+    profile = get_object_or_404(Profile, pk=pk)
+    top_skills = profile.skill_set.exclude(description__exact="") #exclude the empty description
+    other_skills = profile.skill_set.filter(description="")
+    context = {'profile':profile, 'top_skills':top_skills, 'other_skills':other_skills}
+    return render(request, 'users/user-profile.html', context)
