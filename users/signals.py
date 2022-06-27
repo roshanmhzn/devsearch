ce5f335c3a23db0b6b40b8db9cd47188b2903264
Maharjan <roshan.maharjan@cotiviti.com>
2022-06-27 23:45:04 +0545

    created signals and connected in apps.py

---
+++ users/signals.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import Profile
+
+# @receiver(post_save, sender=Profile)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username = user.username,
+            email = user.email,
+            name = user.first_name
+        )
+
+
+def delete_user(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+    print("User is deleted")
+
+post_save.connect(create_profile, sender=User)
+post_delete.connect(delete_user, sender=Profile)
